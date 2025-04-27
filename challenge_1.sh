4b8d8e1ab79abd3a414485292494c23e996ee411
howardphan99 <howardphan99@gmail.com>
2025-04-27 05:24:07 +0000

    first challenge done

--- challenge_1.sh
+++ challenge_1.sh
@@ -1,1 +1,3 @@
-echo "Replace this line with your answer to Challenge #1"
+# I research the method from stack overflow for some help
+# the link is "https://stackoverflow.com/questions/30756273/create-text-file-using-echo-in-command-prompt"
+echo "Hello, my name is Hao Phan and I am on my pathway to learn cybersecurity!" > \intro.txt
